96e1e9eff76680604957c666ee9024d34b106d20
Will Cox <wicox@davidson.edu>
2024-03-27 15:59:07 -0400

    Added movie cards to recommendations

--- frontend/src/pages/top-recommendations.js
+++ frontend/src/pages/top-recommendations.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import "../App.css";
 
 function TopRecommendations() {
     const [movieData, setMovieData] = useState([]);
@@ -20,17 +24,42 @@ function TopRecommendations() {
     };
 
     return (
-        <div>
-            {/* <button onClick={fetchRecommendations}>Load Recommendations</button> */}
-            <Button variant="primary" onClick={fetchRecommendations}>Recommend</Button>
+        <div className="recommend-wrapper">
+            <Button variant="primary" onClick={fetchRecommendations} className="recommend-button">Recommend</Button>
             {(movieData != []) && (
-                <ul className="App-header">
+                // <ul className="recommendations">
+                // <ListGroup horizontal={'md'}>
+                //     {Object.keys(movieData).map((key, index) => (
+                //         // <li key={index}>
+                //         //     <Link to={`/movie/${movieData[key].title}`}>{movieData[key].title}</Link>
+                //         // </li>
+                //         <ListGroup.Item>
+                //             <Link to={`/movie/${movieData[key].title}`}>{movieData[key].title}</Link>
+                //         </ListGroup.Item>
+                //     ))}
+                // </ListGroup>
+                <CardGroup>
                     {Object.keys(movieData).map((key, index) => (
-                        <li key={index}>
-                            <Link to={`/movie/${movieData[key].title}`}>{movieData[key].title}</Link>
-                        </li>
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                        <Card.Body>
+                            <Card.Title>{movieData[key].title}</Card.Title>
+                            <Card.Text>
+                                {movieData[key].description}  
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>{movieData[key].genre}</ListGroup.Item>
+                            {/* <ListGroup.Item>{movieData[key].keywords}</ListGroup.Item> */}
+                            <ListGroup.Item><Link to={`/movie/${movieData[key].title}`}>{movieData[key].title}</Link></ListGroup.Item>
+                        </ListGroup>
+                        {/* <Card.Body>
+                          <Card.Link href="#">Card Link</Card.Link>
+                          <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body> */}
+                      </Card>
                     ))}
-                </ul>
+                </CardGroup>
             )}
         </div>
     );

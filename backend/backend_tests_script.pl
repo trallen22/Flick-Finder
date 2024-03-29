221e51cf1bbe0cba00ad5d390c418578eb7ef248
trallen22 <77846813+trallen22@users.noreply.github.com>
2024-03-29 00:58:01 -0400

    made null file a variable to change between OS

--- backend/backend_tests_script.pl
+++ backend/backend_tests_script.pl
@@ -4,6 +4,7 @@ use Cwd;
 my $curTest;
 our $passedTests = 0;
 our $numTests = 0;
+my $nullFile = '/dev/null'; # change to NUL for Windows -> my $nullFile = 'NUL'
 
 sub check_test {
     my $curStr = shift;
@@ -20,19 +21,19 @@ sub check_test {
 # Section 1: Signup api
 #######################
 # Test 1.1: this tests signs a user up; should return success 
-$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "username": "ta2", "password": "123", "email": "test2\@email.com" }\' -H "Content-Type: application/json" 2>/dev/null | grep success`;
+$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "username": "ta2", "password": "123", "email": "test2\@email.com" }\' -H "Content-Type: application/json" 2>$nullFile | grep success`;
 check_test($curTest, 1.1);
 
 # Test 1.2: this test doesn't include username; should return failure with error message "missing username" 
-$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "password": "123", "email": "test\@email.com" }\' -H "Content-Type: application/json" 2>/dev/null | grep failed`;
+$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "password": "123", "email": "test\@email.com" }\' -H "Content-Type: application/json" 2>$nullFile | grep failed`;
 check_test($curTest, 1.2);
 
 # Test 1.3: this test doesn't include password; should return failure with error message "missing password" 
-$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "username": "ta2", "email": "test2\@email.com" }\' -H "Content-Type: application/json" 2>/dev/null | grep failed`;
+$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "username": "ta2", "email": "test2\@email.com" }\' -H "Content-Type: application/json" 2>$nullFile | grep failed`;
 check_test($curTest, 1.3);
 
 # Test 1.4: this test doesn't include email; should return failure with error message "missing email" 
-$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "username": "ta2", "password": "123" }\' -H "Content-Type: application/json" 2>/dev/null | grep failed`;
+$curTest = `curl -X POST http://127.0.0.1:5000/sign-up -d \'{ "username": "ta2", "password": "123" }\' -H "Content-Type: application/json" 2>$nullFile | grep failed`;
 check_test($curTest, 1.4);
 
 
@@ -40,14 +41,17 @@ check_test($curTest, 1.4);
 # Section 2: Login api
 ###################### 
 # Test 2.1: this tests logging in a real user; should return success 
-$curTest = `curl -X POST http://127.0.0.1:5000/login -d \'{ "username": "ta1", "password": "123" }\' -H "Content-Type: application/json" 2>/dev/null | grep success`;
+$curTest = `curl -X POST http://127.0.0.1:5000/login -d \'{ "username": "ta1", "password": "123" }\' -H "Content-Type: application/json" 2>$nullFile | grep success`;
 check_test($curTest, 2.1);
 
 # Test 2.2: this tests logging in with bad credentials; should return failure with error message "user not found" 
-$curTest = `curl -X POST http://127.0.0.1:5000/login -d \'{ "username": "NOTta1", "password": "123" }\' -H "Content-Type: application/json" 2>/dev/null | grep failed`;
+$curTest = `curl -X POST http://127.0.0.1:5000/login -d \'{ "username": "NOTta1", "password": "123" }\' -H "Content-Type: application/json" 2>$nullFile | grep failed`;
 check_test($curTest, 2.2);
 
 
 END {
-    print("Tests passed: $passedTests/$numTests\n")
+    print("Tests passed: $passedTests/$numTests\n");
+    if (!$passedTests) {
+        print("Make sure the backend server is running\n");
+    }
 }

874eb8f6a0571354f3738ab641d6cdf36e0b63fb
Will Cox <wicox@davidson.edu>
2024-04-03 19:45:28 -0400

    Committing venv changes

--- backend/user 2.py
+++
@@ -1,7 +0,0 @@
-from flask_login import UserMixin
-
-class User(UserMixin):
-    def __init__(self, id, name, active=True):
-        self.id = id
-        self.name = name
-        self.active = active

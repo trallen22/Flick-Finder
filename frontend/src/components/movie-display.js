72a015f72f18b09e14b06af7c6d1538601e58fa5
Will Cox <wicox@davidson.edu>
2024-04-03 21:42:12 -0400

    Updated movie cards to only display movie posters

--- frontend/src/components/movie-display.js
+++ frontend/src/components/movie-display.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from 'react-router-dom';
 
 const MovieCardGroup = ({ movieData }) => {
   return (
     <CardGroup>
       {Object.keys(movieData).map((key, index) => (
-        <Card key={index} style={{ width: '20%' }}>
-          {movieData[key].posterURL && <Card.Img variant="top" className="movie-poster" src={movieData[key].posterURL} />}
-          <Card.Body>
-            <Card.Title>{movieData[key].title}</Card.Title>
-            <Card.Text>{movieData[key].description}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>{movieData[key].genre}</ListGroup.Item>
-            {/* <ListGroup.Item>{movieData[key].keywords}</ListGroup.Item> */}
-            <ListGroup.Item><Link to={`/movie/${movieData[key].title}`}>{movieData[key].title}</Link></ListGroup.Item>
-          </ListGroup>
-        </Card>
+        <Link key={index} to={`/movie/${movieData[key].title}`} style={{ textDecoration: 'none' }}>
+          <Card style={{ width: '16rem', marginBottom: '20px' }}>
+            {movieData[key].posterURL && <Card.Img variant="top" src={movieData[key].posterURL} />}
+            {/* <Card.Body> */}
+              {/* <Card.Title>{movieData[key].title}</Card.Title>
+              <Card.Text>{movieData[key].description}</Card.Text> */}
+            {/* </Card.Body> */}
+          </Card>
+        </Link>
       ))}
     </CardGroup>
   );
 };
 
 export default MovieCardGroup;
+
